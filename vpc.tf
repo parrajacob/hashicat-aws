750748f53d9ccaf1418145d99b57005ad87f8602
Jacob Parra <jacobisraelparra@gmail.com>
2020-06-10 11:46:42 -0700

    Corrected issues

--- vpc.tf
+++ vpc.tf
@@ -1,12 +1,13 @@
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  
+  source  = "app.terraform.io/sf-jacob-parra-training/vpc/aws"
 
-  name = "hashicat-database"
+  name = "database-vpc"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1c"]
-  private_subnets = ["10.0.10.0/24"]
-  public_subnets  = ["0.0.0.0/0"]
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
