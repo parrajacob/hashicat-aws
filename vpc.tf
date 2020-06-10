7d40025c7549cb2f1c8932b21406d7282e008076
Jacob Parra <jacobisraelparra@gmail.com>
2020-06-10 11:29:58 -0700

    Fixed azs and subnets

--- vpc.tf
+++ vpc.tf
@@ -4,9 +4,9 @@ module "vpc" {
   name = "hashicat-database"
   cidr = "10.0.0.0/16"
 
-  azs             = ["eu-west-1a", "eu-west-1b", "eu-west-1c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs             = ["us-east-1"]
+  private_subnets = ["10.0.10.0/24", "10.0.0.0/16"]
+  public_subnets  = ["10.0.10.0/24", "10.0.0.0/16"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
