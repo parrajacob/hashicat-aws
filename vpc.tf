8b069997ba464a102df68771a1010e8b6c358a31
Jacob Parra <jacobisraelparra@gmail.com>
2020-06-10 11:41:11 -0700

    Fixed Subnets

--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,17 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  cidr_block           = var.address_space
-  enable_dns_hostnames = true
+  name = "hashicat-database"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1c"]
+  private_subnets = ["10.0.10.0/24"]
+  public_subnets  = ["0.0.0.0/0"]
+
+  enable_nat_gateway = true
+  enable_vpn_gateway = true
 
   tags = {
-    name = "database-vpc"
     Terraform = "true"
     Environment = "dev"
   }
