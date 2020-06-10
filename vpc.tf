fdffe62197104d6405e9e21f17bca1c7756f14c9
Jacob Parra <jacobisraelparra@gmail.com>
2020-06-10 11:38:23 -0700

    Update to use rest of chart.

--- vpc.tf
+++ vpc.tf
@@ -1,17 +1,11 @@
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
 
-  name = "hashicat-database"
-  cidr = "10.0.0.0/16"
-
-  azs             = ["us-east-1c"]
-  private_subnets = ["10.0.10.0/24", "10.0.0.0/16"]
-  public_subnets  = ["10.0.10.0/24", "10.0.0.0/16"]
-
-  enable_nat_gateway = true
-  enable_vpn_gateway = true
+  cidr_block           = var.address_space
+  enable_dns_hostnames = true
 
   tags = {
+    name = "database-vpc"
     Terraform = "true"
     Environment = "dev"
   }
